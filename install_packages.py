91f2f3e7e4f26c705e1d2246ff54d0bb45b3c335
AcylSilane <24254612+AcylSilane@users.noreply.github.com>
2021-05-22 15:07:52 -0700

    Add docstring to install-packages

--- install_packages.py
+++ install_packages.py
@@ -1,6 +1,10 @@
+"""
+This file installs the packages listed in requirements-dev.txt
+using the project's sys.executable.
+"""
+
 import sys
 import subprocess
-
 
 with open("requirements-dev.txt", "r") as inp:
     packages = [line.strip() for line in inp]
