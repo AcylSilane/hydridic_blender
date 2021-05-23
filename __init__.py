5738f07b22484e72f1372d30b66b66bcc4b095df
AcylSilane <24254612+AcylSilane@users.noreply.github.com>
2021-05-23 14:28:04 -0700

    Add a basic import menu

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import bpy
+import bpy_extras
 
 import os
 import sys
@@ -13,7 +14,7 @@ bl_info = {
     "description": "Import and manipulate molecules, crystals, and other chemical species in an efficient manner",
     "blender": (2, 80, 0),
     "version": (0, 1, 0),
-    "location": "File -> Import -> Chemical",
+    "location": "File > Import > Chemical Structure",
     "warning": "",
     "tracker_url": "https://github.com/AcylSilane/hydridic_blender/issues",
     "category": "Import-Export",
@@ -21,6 +22,17 @@ bl_info = {
 
 # =================
 # Addon Preferences
+
+# For some reason, the only way to get the "Install Dependencies" button to show up on the
+# preferences menu is to inline it here. Definining the install_dependencies and preferences
+# classes in a separate file and importing them is not enough. They *have* to be defined in
+# here for them to register.
+#
+# An alternative I explored here was defining them in a separate file, importing them (for the
+# benefit of my IDE's linter), and then running exec on the file they were originally defined
+# in. My aversion to exec exceeds my aversion to defining random things in a package's __init__.
+
+DEPENDENCIES = ("ase",)
 
 
 class HYDRIDIC_OT_install_dependencies(bpy.types.Operator):
@@ -33,8 +45,6 @@ class HYDRIDIC_OT_install_dependencies(bpy.types.Operator):
     The user accesses this operator via the addon preferences menu.
     """
 
-    dependencies = ("ase",)
-
     bl_idname = "hydridic.install_dependencies"
     bl_label = "Install Dependencies (May take several minutes)"
     bl_description = (
@@ -46,16 +56,14 @@ class HYDRIDIC_OT_install_dependencies(bpy.types.Operator):
 
     @classmethod
     def dependencies_installed(cls) -> bool:
-        return all(
-            importlib.util.find_spec(dependency) for dependency in cls.dependencies
-        )
+        return all(importlib.util.find_spec(dependency) for dependency in DEPENDENCIES)
 
     @classmethod
     def poll(self, context: bpy.types.Context) -> bool:
         return not self.dependencies_installed()
 
     def execute(self, context: bpy.types.Context):
-        subprocess.call([sys.executable, "-m", "pip", "install", *self.dependencies])
+        subprocess.call([sys.executable, "-m", "pip", "install", *DEPENDENCIES])
         return {"FINISHED"}
 
 
@@ -73,12 +81,50 @@ class HYDRIDIC_UL_preferences(bpy.types.AddonPreferences):
 
 classes = (HYDRIDIC_OT_install_dependencies, HYDRIDIC_UL_preferences)
 
+# ==============
+# Menu Functions
+
+
+class HYDRIDIC_OT_import_chemical_structure(
+    bpy.types.Operator, bpy_extras.io_utils.ImportHelper
+):
+    """Import a chemical structure into Blender. Supports all ASE-supported formats."""
+
+    bl_idname = "hydridic.import_chemical_structure"
+    bl_label = "Import Chemical"
+
+    filename_ext = ".xyz"
+
+    @classmethod
+    def poll(self, context):
+        return True
+
+    def execute(self, context):
+        print(self.properties.filepath)
+        return {"FINISHED"}
+
+
+classes += (HYDRIDIC_OT_import_chemical_structure,)
+
+
+def menu_func_import(self, context):
+    layout = self.layout
+    layout.operator(
+        HYDRIDIC_OT_import_chemical_structure.bl_idname, text="Chemical Structure"
+    )
+
+
+# =======================
+# Register those classes!
+
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
