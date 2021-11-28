8aeb217cba57bcbe03109458b0a4ecb674370de6
AcylSilane <24254612+AcylSilane@users.noreply.github.com>
2021-11-28 13:08:22 -0800

    Refactor mock chemical and bondbag into fixtures

--- test/test_bond.py
+++ test/test_bond.py
@@ -3,38 +3,40 @@ Tests functionality related to bonds
 """
 import sys
 import typing
-if typing.TYPE_CHECKING:
-    import scipy.sparse
 
 import pytest
 import mock
 
 from fixtures import molecule_ethanol, molecule_ethanol_bonds
 import config
+
 sys.path.append(config.project_root)
 
 from utils.bond import BondBag
 
-def test_bondbag_caches_bondlist(molecule_ethanol):
-    chemical = mock.Mock(atoms=molecule_ethanol)
-    bag = BondBag(chemical)
 
-    assert bag._has_calculated_bonds is False and bag._bonds == []
-    throwaway = bag.bonds
-    assert bag._has_calculated_bonds is True and len(bag._bonds) > 0
+@pytest.fixture()
+def mock_chemical(molecule_ethanol):
+    yield mock.Mock(atoms=molecule_ethanol)
 
-def test_bondbag_calculates_adjacency(molecule_ethanol, molecule_ethanol_bonds):
-    chemical = mock.Mock(atoms=molecule_ethanol)
-    bag = BondBag(chemical)
 
-    calculated_bonds = bag.adjacency_matrix
+@pytest.fixture()
+def bond_bag(mock_chemical):
+    yield BondBag(mock_chemical)
+
+
+def test_bondbag_caches_bondlist(bond_bag):
+    assert bond_bag._has_calculated_bonds is False and bond_bag._bonds == []
+    throwaway = bond_bag.bonds
+    assert bond_bag._has_calculated_bonds is True and len(bond_bag._bonds) > 0
+
+
+def test_bondbag_calculates_adjacency(bond_bag, molecule_ethanol_bonds):
+    calculated_bonds = bond_bag.adjacency_matrix
     difference = calculated_bonds - molecule_ethanol_bonds
     # If subtracting the matrices from one-another leads to all 0's, they must be the same
     assert difference.nnz == 0
 
-def test_bondbag_len(molecule_ethanol, molecule_ethanol_bonds):
-    chemical = mock.Mock(atoms=molecule_ethanol)
-    bag = BondBag(chemical)
 
-    assert len(bag) == molecule_ethanol_bonds.nnz
-
+def test_bondbag_len(bond_bag, molecule_ethanol_bonds):
+    assert len(bond_bag) == molecule_ethanol_bonds.nnz
